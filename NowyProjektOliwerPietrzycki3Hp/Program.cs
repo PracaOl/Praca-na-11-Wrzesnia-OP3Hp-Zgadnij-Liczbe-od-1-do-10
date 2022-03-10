f1d0d8d04dbd2dbd47c715033a153aa450c54e63
3109362 <3109362@TE-GDA-D-D37H06>
2022-03-10 12:57:56 +0100

    Dodaj pliki projektów.

---
+++ NowyProjektOliwerPietrzycki3Hp/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// OP3Hp
+double y;
+
+Random rnd = new Random();
+int x = rnd.Next(1, 10);
+
+do
+{
+	Console.WriteLine("Podaj liczbę od 1 do 10:");
+	string liczba = Console.ReadLine();
+	y = double.Parse(liczba);
+
+
+	if (y < x)
+	{
+		Console.WriteLine("Podana liczba jest mniejsza od wylosowanej liczby.");
+	}
+
+	if (y > x)
+	{
+		Console.WriteLine("Podana liczba jest wieksza od wylosowanej liczby.");
+	}
+
+} while (y != x);
+
+if (y == x)
+{
+	Console.WriteLine("Zgadles! Jest to:" + x);
+}
